69e5e004cba91e279cc6b78fc4eec64136bec4cc
unknown <Administrator@LDL-SANJUKTA.impelsys.com>
2019-07-21 13:27:22 +0530

    Updates

---
+++ precache-manifest.e9d9cae5f7c3741f331ffa3398bc3403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f7d5a4c29e79e919c5bf4ac6ccfede",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "da07b1764ba3a0c4dcc9",
+    "url": "/Monster-rolodex/static/css/main.fd4b4b9b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "da07b1764ba3a0c4dcc9",
+    "url": "/Monster-rolodex/static/js/main.12c9c41e.chunk.js"
+  },
+  {
+    "revision": "307cc04fdb47796ae357",
+    "url": "/Monster-rolodex/static/js/runtime~main.d0767bc0.js"
+  }
+]);
